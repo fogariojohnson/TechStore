9cfcfcae497b043afc8cbe7cfc1d159ff51ec1ca
Frelin C. Ogario Johnson <125404432+fogariojohnson@users.noreply.github.com>
2023-05-26 23:21:54 -0500

    Update promotions.py

--- promotions.py
+++ promotions.py
@@ -1,60 +1,127 @@
-from abc import ABC, abstractmethod
+import sys
+import products
+import store
+import promotions
 
 
-class Promotion(ABC):
-    """Represent the promotion applied for a certain product."""
-    name = "Promotion"
+class TechStore:
+    """
+       Represents a Best Buy store.
 
-    # A method imposed in all the child class.
-    @abstractmethod
-    def apply_promotion(self, product, quantity):
-        """Returns the discounted price after promotion was applied."""
-        pass
+       Encapsulates the functionality related to managing a store in the Best Buy store.
+       It provides methods for listing products, checking total quantities, making orders, and more.
+
+       Attributes:
+           product_list (list): List of products representing the products available in the store.
+    """
+    def __init__(self):
+        self.product_list = [
+            products.Product("MacBook Air M2", price=1450, quantity=100),
+            products.Product("Bose QuietComfort Earbuds", price=250, quantity=500),
+            products.Product("Google Pixel 7", price=500, quantity=250),
+            products.NonStockedProduct("Windows License", price=125),
+            products.LimitedProduct("Shipping", price=10, quantity=250, maximum=1)
+        ]
+
+        # Create promotion catalog
+        second_half_price = promotions.SecondHalfPrice("Second Half price!")
+        third_one_free = promotions.ThirdOneFree("Third One Free!")
+        thirty_percent = promotions.PercentDiscount("30% off!")
+
+        # Add promotions to products
+        self.product_list[0].set_promotion(second_half_price)
+        self.product_list[1].set_promotion(third_one_free)
+        self.product_list[3].set_promotion(thirty_percent)
+
+        self.best_buy = store.Store(self.product_list)
+
+    def show_list(self):
+        """
+        Shows the list of products in the store with their price and quantity.
+        Updates the quantity when orders are made.
+        """
+        products_list = self.best_buy.products
+        print("______")
+        for index, product in enumerate(products_list, start=1):
+            print(f"Product #{index}: {product.show()}")
+        print("______")
+
+    def total_quantity(self):
+        """
+        Shows the total quantity available in the store.
+        Updates the total quantity when orders are made.
+        """
+        total_quantity = self.best_buy.get_total_quantity()
+        print(f"Total of {int(total_quantity)} items in store")
+
+    def make_order(self):
+        """Takes orders until the user enters an empty text."""
+        order_list = []
+        self.show_list()
+        print("When you want to finish order, enter empty text.")
+
+        while True:
+            product_num = input("Which product # do you want? ")
+            if not product_num:
+                break
+
+            try:
+                product_num = int(product_num)
+                if product_num < 1 or product_num > len(self.best_buy.products):
+                    raise ValueError("Invalid product number. Please try again.")
+            except ValueError as error:
+                print(str(error))
+                continue
+
+            quantity = input("What amount do you want? ")
+            try:
+                quantity = int(quantity)
+                if quantity <= 0:
+                    raise ValueError("Invalid quantity. Please enter a positive quantity.")
+            except ValueError as error:
+                print(str(error))
+                continue
+
+            product = self.best_buy.products[product_num - 1]
+            order_list.append((product, quantity))
+            print("Product added to list!")
+
+        try:
+            total_price = sum(product.buy(quantity) for product, quantity in order_list)
+            formatted_price = f"{total_price:.2f}"
+            print(f"Order made! Total payment: ${formatted_price}")
+        except ValueError as error:
+            print(str(error))
+
+    @staticmethod
+    def exit_program():
+        """Exits the program."""
+        sys.exit()
+
+    def start(self):
+        """ Displays a menu and allows the user to choose from a list of options."""
+        while True:
+            try:
+                print("  Store Menu\n  ------- \n1. List all products in store\n"
+                      "2. Show total amount in store\n3. Make an order\n4. Quit")
+
+                functions = {
+                    1: self.show_list,
+                    2: self.total_quantity,
+                    3: self.make_order,
+                    4: self.exit_program,
+                }
+
+                user_choice = int(input("Please choose a number: "))
+                if user_choice in functions:
+                    functions[user_choice]()
+                    self.start()
+
+            except ValueError:
+                print("\033[31m" + "Error! Please choose from numbers 1 to 4." + "\033[0m")
+                self.start()
 
 
-class SecondHalfPrice(Promotion):
-    """Represents the discount for the second quantity for half the price."""
-    def __init__(self, name):
-        self.name = name
-
-    def apply_promotion(self, product, quantity):
-        # Calculate the discounted price for the second item at half price promotion
-        # For every two items, one item will be at half price
-        discounted_price = 0.0
-        if quantity >= 2:
-            full_price_quantity = quantity // 2  # Number of items at full price
-            half_price_quantity = quantity - full_price_quantity  # Number of items at half price
-            discounted_price = (full_price_quantity * product.price) + (half_price_quantity * product.price / 2)
-        else:
-            discounted_price = product.price * quantity
-
-        return discounted_price
-
-
-class ThirdOneFree(Promotion):
-    """Represents the discount for the third quantity for free."""
-    def __init__(self, name):
-        self.name = name
-
-    def apply_promotion(self, product, quantity):
-        # Calculate the discounted price for the buy 2, get 1 free promotion
-        # For every three items, one item will be free
-        full_price_quantity = quantity // 3  # Number of items at full price
-        free_quantity = quantity - full_price_quantity  # Number of free items
-        discounted_price = full_price_quantity * product.price
-
-        return discounted_price
-
-
-class PercentDiscount(Promotion):
-    """Represents the discount indicated in the percent."""
-    def __init__(self, name, percent):
-        self.name = name
-        self.percent = percent
-
-    def apply_promotion(self, product, quantity):
-        # Calculate the discounted price for the percentage discount promotion
-        discount = self.percent / 100.0
-        discounted_price = product.price * quantity * (1 - discount)
-
-        return discounted_price
+if __name__ == "__main__":
+    store = TechStore()
+    store.start()
